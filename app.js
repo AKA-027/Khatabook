b7520a8d011620ce98aa6ea5c696e5dc29b2e92b
Saumya Saxena <saxenasaumya027@gmail.com>
2026-01-18 16:04:56 +0530

    renamed user model file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express") ;
 const app = express() ;
 const path = require("path") ;
 const mongoose = require("mongoose") ;
-const userModel = require("./models/userModel") ;
+const userModel = require("./models/user") ;
 const bcrypt = require("bcrypt") ;
 const jwt = require("jsonwebtoken") ;
 const cookieParser = require("cookie-parser") ;
@@ -12,6 +12,7 @@ app.use(express.json()) ;
 app.use(express.urlencoded({extended:true})) ;
 app.use(express.static(path.join(__dirname , "public"))) ;
 app.set("view engine" , "ejs") ;
+app.use(cookieParser()) ;
 
 mongoose.connect(process.env.MONGODB_URL) 
  .then( () =>{
